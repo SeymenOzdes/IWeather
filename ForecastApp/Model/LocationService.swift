3f2b784761716b4f5e992217e5b42d2a344678a0
Seymen Ozdes <ozdesxseymen@gmail.com>
2025-04-07 18:35:57 +0300

    Add LocationService class for handling location updates and permissions
    - Implemented LocationService with CLLocationManager to request and manage location updates.
    - Added LocationServiceDelegate protocol to notify subscribers of location changes and errors.
    - Configured location accuracy and distance filter for efficient updates.
    - Included permission handling with requestWhenInUseAuthorization and authorization status checks.
    - Stored last known location for future access.

--- ForecastApp/Model/LocationService.swift
+++ ForecastApp/Model/LocationService.swift
@@ -8,15 +8,91 @@
 import Foundation
 import CoreLocation
 
-class LocationService: NSObject, CLLocationManagerDelegate{
+protocol LocationServiceDelegate: AnyObject {
+    func locationService(_: LocationService, didUpdateLocation Location: CLLocation)
+    func locationService(_: LocationService, didFailWithError: Error)
+}
+
+class LocationService: NSObject {
     let locationManager = CLLocationManager()
+    weak var delegate: LocationServiceDelegate?
+    private(set) var lastLocation: CLLocation?
     
     override init() {
         super.init()
+        prepareLocationManager()
+    }
+}
+
+extension LocationService {
+    private func prepareLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 1000 // 1km'lik hareket etmediğin sürece konum güncellemesi yapma
     }
+    
     func requestLocation() {
+        locationManager.requestLocation()
+    }
+    
+    func stopRequestLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func requestLocationPermission() {
+        let authorizationStatus: CLAuthorizationStatus
         
+        if #available(iOS 14.0, *) {
+            authorizationStatus = locationManager.authorizationStatus
+        } else {
+            authorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        
+        switch authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            
+        case .authorizedAlways, .authorizedWhenInUse:  // İzin var, konumu alabiliriz
+            requestLocation()
+            
+        case .denied, .restricted: // Kullanıcı izin vermemiş veya kısıtlamış
+            let error = NSError(domain: "LocationServiceErrorDomain",
+                                code: 1,
+                                userInfo: [NSLocalizedDescriptionKey: "Konum izni verilmedi. Lütfen ayarlardan izin verin."])
+            delegate?.locationService(self, didFailWithError: error)
+        
+       @unknown default:
+            break
+        }
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+            
+            case .authorizedAlways, .authorizedWhenInUse:
+                requestLocation()
+            
+        case .denied, .restricted:
+            let error = NSError(domain: "LocationServiceErrorDomain",
+                                code: 1,
+                                userInfo: [NSLocalizedDescriptionKey: "Konum izni verilmedi. Lütfen ayarlardan izin verin."])
+            delegate?.locationService(self, didFailWithError: error)
+       
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        delegate?.locationService(self, didFailWithError: error)
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        lastLocation = location
+        delegate?.locationService(self, didUpdateLocation: location)
     }
 }
